78255cb5327fd2fca66454c3dc1e14da60f8be9e
c4rls <ssantos.carloseduardo@gmail.com>
2020-05-11 18:54:14 -0300

    Adding the main application screen.

--- app/src/main/java/com/example/bin2dec/MainActivity.kt
+++ app/src/main/java/com/example/bin2dec/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.bin2dec
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,4 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun printResult(view: View) {
+        val viewResult = findViewById<TextView>(R.id.result)
+        val viewInput = findViewById<EditText>(R.id.input)
+
+        val input = viewInput.text.toString()
+        val error = validateInput(input)
+
+        if(error ==  null) viewResult.text = binaryToDecimal(input).toString()
+    }
 }
+
--- app/src/main/java/com/example/bin2dec/utils.kt
+++ app/src/main/java/com/example/bin2dec/utils.kt
@@ -2,21 +2,6 @@ package com.example.bin2dec
 
 import kotlin.math.pow
 
-fun main(args: Array<String>) {
-    val number: String = if(args.isEmpty()){
-        readLine().toString()
-    } else args[0]
-
-    val indexFail = number.indexOfFirst { char -> char != '1' && char != '0' }
-    if(indexFail > -1) {
-        println("\"${number[indexFail]}\" is not a valid digit")
-        return
-    }
-
-    println(binaryToDecimal(number))
-
-}
-
 
 fun binaryToDecimal(binaryNumber: String): Int {
     var decimalNumber = 0
@@ -25,4 +10,11 @@ fun binaryToDecimal(binaryNumber: String): Int {
         decimalNumber += (2.toDouble().pow(index.toDouble()) * Character.getNumericValue(char)).toInt()
     }
     return decimalNumber
+}
+
+fun validateInput(binaryNumber: String): Char? {
+    val indexFail = binaryNumber.indexOfFirst { char -> char != '1' && char != '0' }
+    if(indexFail > -1) return binaryNumber[indexFail]
+
+    return null
 }
