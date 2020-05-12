2eedf2faba16777f48a237a2d0a04343f5ee9865
c4rls <ssantos.carloseduardo@gmail.com>
2020-05-12 09:50:24 -0300

    Shows a popup alert if the user does not enter a valid digit.

--- app/src/main/java/com/example/bin2dec/MainActivity.kt
+++ app/src/main/java/com/example/bin2dec/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +23,15 @@ class MainActivity : AppCompatActivity() {
         val error = validateInput(input)
 
         if(error ==  null) viewResult.text = binaryToDecimal(input).toString()
+        else showAlert(R.string.invalid_digit)
+    }
+
+    private fun showAlert(stringId: Int) {
+        Snackbar.make(
+            findViewById(R.id.activityMain),
+            stringId,
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 }
 
